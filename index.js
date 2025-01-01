6dfb16c59447436e2b406e21fe1f351ea91ace2d
thirupathi8 <124600966+thirupathi8@users.noreply.github.com>
2025-01-01 22:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.options('*', cors({
-  origin: 'https://notemaker-7m7w4e4w8-thirupathi8s-projects.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-// app.use(cors());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.url}`);
